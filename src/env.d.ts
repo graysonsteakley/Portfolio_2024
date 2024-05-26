9a0aca656441ae95d7fac191c8e921ef9830824a
Grayson Steakley <grayson.steakley@gmail.com>
2024-05-26 12:32:49 -0500

    CHANGE: added mailing

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly TO_EMAIL: string;
+  readonly TO_EMAIL_PASSWORD: string;
+  // more env variables...
+}
--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,4 +1,6 @@
 import type { APIRoute } from "astro";
+import nodemailer from "nodemailer";
+import type { Address } from "nodemailer/lib/mailer";
 
 // IF A ROUTE SHOULD BE SSR NOT SSG
 export const prerender = false;
@@ -18,11 +20,41 @@ export const POST: APIRoute = async ({ request }) => {
     );
   }
 
-  // Do something with the data, then return a success response
-  return new Response(
-    JSON.stringify({
-      message: "Successfully sent message!",
-    }),
-    { status: 200 },
-  );
+  const toEmail = import.meta.env.TO_EMAIL;
+  const toEmailPassword = import.meta.env.TO_EMAIL_PASSWORD;
+
+  const mailOptions = {
+    from: email as unknown as Address,
+    to: toEmail,
+    subject: "New Portfolio Site Contact Form Submission!",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: toEmail,
+      pass: toEmailPassword,
+    },
+  });
+
+  // Send the email
+  try {
+    await transporter.sendMail(mailOptions);
+    return new Response(
+      JSON.stringify({
+        message: "Successfully sent message!",
+      }),
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({
+        message: "Failed to send message",
+        error,
+      }),
+      { status: 500 },
+    );
+  }
 };
