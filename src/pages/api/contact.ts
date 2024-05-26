b448cf8cb29f246475a4b6579af6e9a8b93dc364
Grayson Steakley <grayson.steakley@gmail.com>
2024-05-26 11:01:37 -0500

    FIX: cleanup

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -6,11 +6,9 @@ export const prerender = false;
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
 
-  console.log("data, ", data);
   const name = data.get("name");
   const email = data.get("email");
   const message = data.get("message");
-  // Validate the data - you'll probably want to do more than this
   if (!name || !email || !message) {
     return new Response(
       JSON.stringify({
@@ -19,6 +17,7 @@ export const POST: APIRoute = async ({ request }) => {
       { status: 400 },
     );
   }
+
   // Do something with the data, then return a success response
   return new Response(
     JSON.stringify({
